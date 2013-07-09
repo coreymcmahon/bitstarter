80a181499cd75dfad5066ae1d2a48f25cb53f059
Corey McMahon <contact@coreymcmahon.com>
2013-07-09 08:09:19 +0000

    Read from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
